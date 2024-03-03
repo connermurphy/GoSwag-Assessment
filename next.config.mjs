d6e1a75bf42b21f4380154121d36384cbba25eb2
Conner <chuckieminster@aol.com>
2024-03-03 16:34:12 +0000

    updated next config + imported logos from figma

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        //allow optimisation of svg, safe to do so if we are controlling svg code
+        //unsafe to do so if users can manipulate or modify svg (xss!)
+        dangerouslyAllowSVG: true
+    }
+};
 
 export default nextConfig;
